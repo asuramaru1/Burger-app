2ecdff82da58ed466f0b03b6f66a17f7b198e21a
asuramaru1 <gkhar17@freeuni.edu.ge>
2021-03-15 13:32:47 +0400

    added burger builder component and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+import React, {Component} from "react";
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+class App extends Component{
+  render (){
+    return (
+        <div >
+          <Layout>
+              <BurgerBuilder/>
+          </Layout>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from "../../hoc/Auxe";
+import classes from "./Layout.module.css";
+
+const layout = (props) => (
+  <Aux>
+    <div>toolbar , sideDrawer , backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+  </Aux>
+);
+export default layout;
